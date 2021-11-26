20ca6141531823e52f38b0117d2de092bb58f6e4
sohail_rasool-786 <sohail.rasool@conovoinc.com>
2021-11-26 19:22:26 +0500

    Add cart Item component

--- src/components/CartDropDown/CartDropDown.js
+++ src/components/CartDropDown/CartDropDown.js
@@ -1,11 +1,18 @@
 import { Button } from 'react-bootstrap';
 import React from 'react';
 import Classes from './CartDropDown.module.css';
+import CartItem from '../CartItem/CartItem';
+import { useSelector } from 'react-redux';
 
 const CartDropDown = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
   return (
     <div className={Classes.cartDropdown}>
-      <div className={Classes.cartItems} />
+      <div className={Classes.cartItems}>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <Button className={Classes.checkOutButton} variant='dark'>
         Check Out
       </Button>
---
+++ src/components/CartItem/CartItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Classes from './CartItem.module.css';
+import { Image } from 'react-bootstrap';
+
+const CartItem = ({ item: { name, imageUrl, price, quantity } }) => {
+  return (
+    <div className={Classes.cartItem}>
+      <Image
+        className={Classes.imgItem}
+        src={imageUrl}
+        alt={name}
+        style={{ objectFit: 'cover' }}
+      />
+      <div className={Classes.itemDetails}>
+        <span className={Classes.nameItem}>{name}</span>
+        <span>
+          {quantity} * {`$ ${price}`}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
