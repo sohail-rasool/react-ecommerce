172fdee5fe2ea39104f398267f4859956b1abf16
sohail_rasool-786 <sohail.rasool@conovoinc.com>
2021-11-29 16:26:31 +0500

    Removed unnecessary files

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { FaBars, FaShoppingCart } from "react-icons/fa";
-import { useSelector, useDispatch } from "react-redux";
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaBars, FaShoppingCart } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { auth } from "../../services/firebase/firebase.utils";
-import { toggleDropDownAction } from "../../store/actions/cartDropDownAction";
+import { auth } from '../../services/firebase/firebase.utils';
+import { toggleDropDownAction } from '../../store/actions/cartDropDownAction';
 import {
   selectCartItemCount,
   selectCartDropDown,
-} from "../../store/selectors/cartSelector";
-import { selectCurrentUser } from "../../store/selectors/userSelector";
+} from '../../store/selectors/cartSelector';
+import { selectCurrentUser } from '../../store/selectors/userSelector';
 
-import SwitchTheme from "../../components/SwitchButton/SwitchTheme";
-import CartDropDown from "../../components/CartDropDown/CartDropDown";
+import SwitchTheme from '../../components/SwitchButton/SwitchTheme';
+import CartDropDown from '../../components/CartDropDown/CartDropDown';
 
-import "./Header.css";
+import './Header.css';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -31,39 +31,39 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar fixed="top" bg="light" expand="lg" className="custom-navbar">
-        <Container className="position-relative">
-          <Navbar.Brand as={Link} to="/">
+      <Navbar fixed='top' bg='light' expand='lg' className='custom-navbar'>
+        <Container className='position-relative'>
+          <Navbar.Brand as={Link} to='/'>
             React-Bootstrap
           </Navbar.Brand>
           <Navbar.Toggle>
             <FaBars />
           </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto align-items-center">
-              <Nav.Link as={Link} to="/shop">
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto align-items-center'>
+              <Nav.Link as={Link} to='/shop'>
                 SHOP
               </Nav.Link>
-              <Nav.Link as={Link} to="/contact">
+              <Nav.Link as={Link} to='/contact'>
                 CONTACT
               </Nav.Link>
 
               {user ? (
                 <Nav.Link onClick={() => auth.signOut()}>SIGN Out</Nav.Link>
               ) : (
-                <Nav.Link as={Link} to="/signin">
+                <Nav.Link as={Link} to='/signin'>
                   SIGN IN
                 </Nav.Link>
               )}
 
               <Nav.Link
-                className="cart-icon position-relative"
+                className='cart-icon position-relative'
                 onClick={toggleDropDown}
               >
                 <FaShoppingCart />({itemCount})
                 {showDropDown && <CartDropDown />}
               </Nav.Link>
-              <SwitchTheme className="ms-2" />
+              <SwitchTheme className='ms-4' />
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/pages/checkout/CheckOut.js
+++ src/pages/checkout/CheckOut.js
@@ -1,34 +1,41 @@
-import React from "react";
-import { Table } from "react-bootstrap";
-import { selectCartTotalAmount,selectCartItems } from "../../store/selectors/cartSelector";
-import { useSelector } from "react-redux";
-import CheckOutItem from '../../components/CheckOutItem/CheckOutItem'
-import "./CheckOut.css";
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import {
+  selectCartTotalAmount,
+  selectCartItems,
+} from '../../store/selectors/cartSelector';
+import { useSelector } from 'react-redux';
+import CheckOutItem from '../../components/CheckOutItem/CheckOutItem';
+import './CheckOut.css';
 const CheckOut = () => {
- const state = useSelector((state)=>state)
- const totalAmout = selectCartTotalAmount(state)
- const cartItems = selectCartItems(state)
+  const state = useSelector((state) => state);
+  const totalAmout = selectCartTotalAmount(state);
+  const cartItems = selectCartItems(state);
   return (
     <>
-    <Table striped bordered hover className='custom-table'>
-      <thead className='text-center'>
-        <tr>
-          <th>Product</th>
-          <th>Description</th>
-          <th>Quantity</th>
-          <th>Price</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          cartItems.map((cartItem) => <CheckOutItem key={cartItem.id} item={cartItem} {...cartItem} />)
-        }
-      </tbody>
-    </Table>
-    <h3 style={{textAlign:'right'}}>
-      Total: {totalAmout}$
-    </h3>
+      {cartItems.length !== 0 ? (
+        <>
+          <Table striped bordered hover className='custom-table'>
+            <thead className='text-center'>
+              <tr>
+                <th>Product</th>
+                <th>Description</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartItems.map((cartItem) => (
+                <CheckOutItem key={cartItem.id} item={cartItem} {...cartItem} />
+              ))}
+            </tbody>
+          </Table>
+          <h3 style={{ textAlign: 'right' }}>Total: {totalAmout}$</h3>
+        </>
+      ) : (
+        <h2 className='text-center'>Cart is empty</h2>
+      )}
     </>
   );
 };
