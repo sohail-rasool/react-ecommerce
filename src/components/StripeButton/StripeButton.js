e34295f9968f06ea01e7d8fd4acf1714a5ec5fb5
sohail_rasool-786 <sohail.rasool@conovoinc.com>
2021-12-01 13:43:46 +0500

    Added stripe payment method

---
+++ src/components/StripeButton/StripeButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_TYooMQauvdEDq54NiTphI7jx';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Online Store'
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
--- src/pages/checkout/CheckOut.js
+++ src/pages/checkout/CheckOut.js
@@ -6,11 +6,13 @@ import {
 } from '../../store/selectors/cartSelector';
 import { useSelector } from 'react-redux';
 import CheckOutItem from '../../components/CheckOutItem/CheckOutItem';
+import StripeButton from '../../components/StripeButton/StripeButton';
 import './CheckOut.css';
 const CheckOut = () => {
   const state = useSelector((state) => state);
   const totalAmout = selectCartTotalAmount(state);
   const cartItems = selectCartItems(state);
+  let months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
   return (
     <>
       {cartItems.length !== 0 ? (
@@ -32,6 +34,14 @@ const CheckOut = () => {
             </tbody>
           </Table>
           <h3 style={{ textAlign: 'right' }}>Total: {totalAmout}$</h3>
+          <div className='text-center'>
+            <StripeButton price={totalAmout} />
+          </div>
+          <p className='mt-5 text-danger text-center'>
+            Test Credit cart Info (4242424242424242 exp:
+            {months[new Date().getUTCMonth()]}/{new Date().getFullYear()} -
+            CW:123)
+          </p>
         </>
       ) : (
         <h2 className='text-center'>Cart is empty</h2>
